684b412b977bc1888188878614b5e81b98edc280
usmhic <usama.hicho.mail@gmail.com>
2022-07-09 12:08:51 +0100

    Update docker_compose.sh

--- docker_compose.sh
+++ docker_compose.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-sudo curl -L "https://github.com/docker/compose/releases/download/latest/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/v2.6.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 docker-compose --version
+
