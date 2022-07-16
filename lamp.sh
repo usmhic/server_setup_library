68636245f9fd1192c3e06d1b99db328be155185f
usmhic <usama.hicho.mail@gmail.com>
2022-07-16 19:55:54 +0100

    Create lamp.sh

---
+++ lamp.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+sudo apt update -y
+sudo apt upgrade -y
+
+
+#nginx
+yes | sudo ufw allow OpenSSH
+yes | sudo ufw enable
+sudo apt install -y apache2
+sudo apt update -y 
+
+#mysql&php
+sudo apt install -y mysql-server
+sudo apt install -y php-fpm php-mysql php-mbstring php-xml php-bcmath php-cli php-curl php-zip unzip
+sudo apt update -y
