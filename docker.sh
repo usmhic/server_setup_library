e151041acfd67920f4a11157c4ad9e1d36aa7700
Oussama Hichou <56653051+usmhic@users.noreply.github.com>
2022-08-06 19:18:53 +0100

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -6,4 +6,3 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 apt-cache policy docker-ce
 sudo apt install -y docker-ce
-sudo systemctl status docker
